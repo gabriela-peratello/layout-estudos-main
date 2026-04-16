901c8794101b99cad4c77adc2697b689e0bab790
gabriela peratello <gabriela.p.silva10@aluno.senai.br>
2026-04-16 11:19:50 -0300

    Iniciando conceitos JavaScript

--- dark-light-mode-v01/script.js
+++ dark-light-mode-v01/script.js
@@ -1,23 +1,12 @@
-// Evento que é disparado quando o conteúdo do DOM (estrutura HTML) é totalmente carregado
+// Capturando os elementos
+// Body -> Controla o tema;
+const body = document.querySelector('.theme__content');
+// button -> Ferramenta que solta a ação;
+const button = document.querySelector('.button__alterar-tema')
 
 
-// Verifica no localStorage se existe um tema salvo. Se não houver, define o tema padrão como 'light'
- 
-
-// Adiciona a classe correspondente ao tema (light ou dark) no corpo da página (body)
-
-
-// Seleciona o botão de alternar tema usando o ID 'toggle-theme' e adiciona um evento de clique
-  
+button.addEventListener('click', ()=> {
+    // Togle ajuda a alterar uma classe sem usar necessariamente um if
+    body.classList.toggle('dark')
     
-// Verifica qual tema está atualmente aplicado no body (light ou dark)
-// Se o tema for 'light', muda para 'dark', caso contrário, muda para 'light'
-    
-
-// Remove as classes de tema antigas ('light' e 'dark') para evitar conflito
-   
-
-// Adiciona a nova classe de tema ao body
-   
-
-// Armazena o novo tema no localStorage para que a preferência do usuário seja mantida
+})
