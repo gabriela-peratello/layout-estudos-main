f1a8d7d74c9eea2795942c33130c88dbfbcae0e6
gabriela peratello <gabriela.p.silva10@aluno.senai.br>
2026-04-24 08:59:15 -0300

    Teste 01- Erros de tipo

--- spa-fetch-components/test/test.js
+++ spa-fetch-components/test/test.js
@@ -1,14 +1,15 @@
 let x =  5;
+let y = 'banana'
 
 try {
-    
+    x = y + 1
 
 } catch(erro){
 
     let text = `
     <strong>Nome do erro:</strong> ${erro.name} <br />
     <strong>Mensagem:</strong> ${erro.message} <br />
-    <strong>Stack:</strong> <pre>${erro.stack}</pre>
+    <strong>Stack:</strong> <span>${erro.stack}</span>
     `;
 
     document.body.innerHTML = text
