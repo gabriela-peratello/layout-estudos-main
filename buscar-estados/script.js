673ef27de513c6b050bb90ff372a59a493092e2a
gabriela peratello <gabriela.p.silva10@aluno.senai.br>
2026-05-07 11:24:31 -0300

    JavaScript arrumado

---
+++ buscar-estados/script.js
@@ -0,0 +1,7 @@
+
+estado.addEventListener('change', async () => {
+
+    const estado = document.getElementById('municipios')
+    fetch(`https://servicodados.ibge.gov.br/api/docs/localidades#api-_`)
+
+})
